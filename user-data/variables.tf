f142e8f445dd1e53a0642d1f6443b4ca99e43352
void269 <void269.269@gmail.com>
2026-07-11 10:16:11 -0400

    added vars file

---
+++ user-data/variables.tf
@@ -0,0 +1,17 @@
+variable "server_port" {
+  description = "SG Inbound port number"
+  type = number
+  default = 80
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type = string
+  default = "us-east-1"
+}
+
+variable "aws_az" {
+  description = "AWS Availability Zone"
+  type = string
+  default = "us-east-1a"
+}
