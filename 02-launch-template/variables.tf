99b73c29b020ffa934a2b7bfe439a81f8bcfc3e3
void269 <void269.269@gmail.com>
2026-07-19 10:21:27 -0400

    fixed ec2 type

--- 02-launch-template/variables.tf
+++ 02-launch-template/variables.tf
@@ -31,7 +31,7 @@ variable "ami_image_id" {
 variable "ec2_instance_type" {
   description = "EC2 Instance Type"
   type = string
-  default = "t2.micro"
+  default = "t3.micro"
 }
 
 variable "access_key" {
